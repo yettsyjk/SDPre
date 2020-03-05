eca76dc342c9fbc84f98e67142baefb6809773e5
yettsyjk <56506858+yettsyjk@users.noreply.github.com>
2020-03-04 19:49:55 -0700

    finish jthw ex 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3 {
+  public static void main (String[] args){
+    System.out.println("Name: Yettsy");
+    System.out.println("Age: 40");
+    System.out.println("State: CA");
+  }
+}
