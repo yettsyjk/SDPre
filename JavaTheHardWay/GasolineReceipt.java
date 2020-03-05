0777138e0e356b8881ca9b123cbcca8a944dc290
yettsyjk <56506858+yettsyjk@users.noreply.github.com>
2020-03-04 18:27:14 -0700

    finish jthw ex 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,16 @@
+public class GasolineReceipt {
+  public static void main (String[] args) {
+    System.out.println("+-----------------------+");
+    System.out.println("|                       |");
+    System.out.println("|    CORNER STORE       |");
+    System.out.println("|    Denver, CO         |");
+    System.out.println("|  2020-03-02 04:38PM   |");
+    System.out.println("|                       |");
+    System.out.println("|  Gallons:      10.870 |");
+    System.out.println("|  Pric/gallon: $ 2.089 |");
+    System.out.println("|                       |");
+    System.out.println("| Fuel total:  $ 22.71  |");
+    System.out.println("|                       |");
+    System.out.println("+-----------------------+");
+  }
+}
---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,6 @@
+public class Objective1Lab2 {
+  public static void main (String[] args){
+    System.out.println("Hello my name is Yettsy");
+    System.out.println("You would like to know my age wouldn't you hahaha, 40");
+  }
+}
